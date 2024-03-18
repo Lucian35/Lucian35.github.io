65733eba138def4defa10057bfa4ec02d8b60f18
Lucian <147434586+Lucian35@users.noreply.github.com>
2024-03-18 10:54:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function changeTheme() {
+  console.log("changeTheme function has been called");
   if (document.body.style.backgroundImage === "url('clouds.png')") {
     document.body.style.backgroundImage = "url('stars.png')";
   }
