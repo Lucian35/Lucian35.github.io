de313e143cba9daef8e9bc4513f75db32d1c2b5e
Lucian <147434586+Lucian35@users.noreply.github.com>
2024-03-18 12:17:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function changeTheme() {
   console.log("changeTheme function has been called");
+  console.log("This is the current background: " + document.body.style.backgroundImage)
   if (document.body.style.backgroundImage === "url('clouds.png')") {
     console.log("the background is clouds. Now changing to stars.");
     document.body.style.backgroundImage = "url('stars.png')";
