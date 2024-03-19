86d09f9a68aa7df77cc43b47100e76e2b389189d
Lucian <147434586+Lucian35@users.noreply.github.com>
2024-03-19 11:31:32 -0700

    still trying to fix dark mode

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 //I have no clue whats going on here
 function changeTheme() {
-  var r = document.querySelector('body::before');
-
-  var rs = getComputedStyle(r);
-  console.log(rs);
-  if (rs.getPropertyValue('--cloudOpacity') === "1") {
-    r.style.setProperty('--cloudOpacity', "0.5");
-    console.log("opacity was one and was changed");
-  }
-  else {
-    r.style.setProperty('--cloudOpacity', "1");
-    console.log("opacity was transparent and was changed");
-  }
+  const htmlElement = document.documentElement;
+  htmlElement.style.setProperty('--cloudOpacity', '0.5');
 }
