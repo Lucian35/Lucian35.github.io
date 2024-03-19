9beb53e0764a9af4500e2a4a1200ebdac6ddd19f
Lucian <147434586+Lucian35@users.noreply.github.com>
2024-03-19 12:01:20 -0700

    Fixed syntax error

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //I have no clue whats going on here but it works. Thank you Claude.
 function changeTheme() {
-  
+
   const htmlElement = document.documentElement;
   const currentOpacity = getComputedStyle(htmlElement).getPropertyValue('--cloudOpacity');
 
   if (currentOpacity === "1"){
     htmlElement.style.setProperty('--cloudOpacity', '0.4');
-    document.getElementsByClassName("mainBox").style.backgroundColor = black;
+    document.getElementsByClassName("mainBox").style.backgroundColor = "black";
   }
   else {
     htmlElement.style.setProperty('--cloudOpacity', '1');
-    document.getElementsByClassName("mainBox").style.backgroundColor = white;
+    document.getElementsByClassName("mainBox").style.backgroundColor = "white";
   }
 }
